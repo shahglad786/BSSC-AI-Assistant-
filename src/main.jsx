89eecf4951104d92669c7f34c8f8da5071850ee6
shahglad786 <shahglad786@gmail.com>
2025-10-23 12:07:22 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
+// This finds the root element and renders the App component.
+// If this file fails, the screen will be blank.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
